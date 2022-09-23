e098c66de915eec6a0c6df6c29f8fbbfc44d0f0b
Anton Khorev <tony29@yandex.ru>
2022-09-23 17:24:53 +0300

    Allow multi fetch request parameter to contain only versioned entries

--- app/controllers/api/elements_controller.rb
+++ app/controllers/api/elements_controller.rb
@@ -4,11 +4,13 @@ module Api
     def index
       raise OSM::APIBadUserInput, "The parameter #{controller_name} is required, and must be of the form #{controller_name}=ID[vVER][,ID[vVER][,ID[vVER]...]]" unless params[controller_name]
 
-      id_ver_strings, id_strings = params[controller_name].split(",").partition { |iv| iv.include? "v" }
+      search_strings = params[controller_name].split(",")
+
+      raise OSM::APIBadUserInput, "No #{controller_name} were given to search for" if search_strings.empty?
+
+      id_ver_strings, id_strings = search_strings.partition { |iv| iv.include? "v" }
       id_vers = id_ver_strings.map { |iv| iv.split("v", 2).map(&:to_i) }
       ids = id_strings.map(&:to_i)
-
-      raise OSM::APIBadUserInput, "No #{controller_name} were given to search for" if ids.empty?
 
       result = current_model.find(ids)
       unless id_vers.empty?
