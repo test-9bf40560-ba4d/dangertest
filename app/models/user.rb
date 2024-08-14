GobinathAL/openstreetmap-website

Remove hardcoded rate limit value. Fixes #4427
This value is the minimum number of changeset comments required to get
Settings.max_changeset_comments_per_hour

app/models/user.rb
@@ -415,8 +415,8 @@ class User < ApplicationRecord
     if moderator?
       Settings.moderator_changeset_comments_per_hour
     else
-      previous_comments = changeset_comments.limit(200).count
-      max_comments = previous_comments / 200.0 * Settings.max_changeset_comments_per_hour
+      previous_comments = changeset_comments.limit(Settings.min_changeset_comments_for_max_rate_limit).count
+      max_comments = previous_comments / Settings.min_changeset_comments_for_max_rate_limit.to_f * Settings.max_changeset_comments_per_hour
       max_comments = max_comments.floor.clamp(Settings.initial_changeset_comments_per_hour, Settings.max_changeset_comments_per_hour)
       max_comments /= 2**active_reports
       max_comments.floor.clamp(Settings.min_changeset_comments_per_hour, Settings.max_changeset_comments_per_hour)

test/controllers/api/changeset_comments_controller_test.rb
@@ -158,7 +158,7 @@ module Api
     def test_create_comment_experienced_user_rate_limit
       changeset = create(:changeset, :closed)
       user = create(:user)
-      create_list(:changeset_comment, 200, :author_id => user.id, :created_at => Time.now.utc - 1.day)
+      create_list(:changeset_comment, Settings.min_changeset_comments_for_max_rate_limit, :author_id => user.id, :created_at => Time.now.utc - 1.day)
 
       auth_header = basic_authorization_header user.email, "test"
 

test-9bf40560-ba4d/dangertest

app/models/user.rb
@@ -415,8 +415,8 @@ class User < ApplicationRecord
     if moderator?
       Settings.moderator_changeset_comments_per_hour
     else
-      previous_comments = changeset_comments.limit(200).count
-      max_comments = previous_comments / 200.0 * Settings.max_changeset_comments_per_hour
+      previous_comments = changeset_comments.limit(Settings.changeset_comments_for_rate_limit).count
+      max_comments = previous_comments / Settings.changeset_comments_for_rate_limit.to_f * Settings.max_changeset_comments_per_hour
       max_comments = max_comments.floor.clamp(Settings.initial_changeset_comments_per_hour, Settings.max_changeset_comments_per_hour)
       max_comments /= 2**active_reports
       max_comments.floor.clamp(Settings.min_changeset_comments_per_hour, Settings.max_changeset_comments_per_hour)

test/controllers/api/changeset_comments_controller_test.rb
@@ -158,7 +158,7 @@ module Api
     def test_create_comment_experienced_user_rate_limit
       changeset = create(:changeset, :closed)
       user = create(:user)
-      create_list(:changeset_comment, 200, :author_id => user.id, :created_at => Time.now.utc - 1.day)
+      create_list(:changeset_comment, Settings.changeset_comments_for_rate_limit, :author_id => user.id, :created_at => Time.now.utc - 1.day)
 
       auth_header = basic_authorization_header user.email, "test"
 