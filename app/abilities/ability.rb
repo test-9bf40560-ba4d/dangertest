AntonKhorev/openstreetmap-website

Add blank 'comments for map edits' page

app/abilities/ability.rb
@@ -15,7 +15,7 @@ class Ability
 
     if Settings.status != "database_offline"
       can [:index, :feed], Changeset
-      can :index, ChangesetComment
+      can [:index, :feed], ChangesetComment
       can [:confirm, :confirm_resend, :confirm_email], :confirmation
       can [:index, :rss, :show, :comments], DiaryEntry
       can [:index], Note

test-9bf40560-ba4d/dangertest

app/abilities/ability.rb
@@ -15,7 +15,7 @@ class Ability
 
     if Settings.status != "database_offline"
       can [:index, :feed, :show], Changeset
-      can :show, ChangesetComment
+      can [:index, :show], ChangesetComment
       can [:confirm, :confirm_resend, :confirm_email], :confirmation
       can [:index, :rss, :show], DiaryEntry
       can :index, DiaryComment